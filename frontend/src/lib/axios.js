8334f33bd4e297d29572743c9a30d151a97b7b48
Sharon Boddu <elinia.sd@icloud.com>
2025-04-06 03:15:33 +0100

    added the backend url pleasw work

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.mode === "development" ? "https://antique-ej8u.onrender.com" : "/api",
     withCredentials: true,
 })
 
