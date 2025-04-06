817787086cca8c16c8e2354df7bc96a1fd8777e2
Sharon Boddu <elinia.sd@icloud.com>
2025-04-06 02:59:26 +0100

    dirname defined

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-
-
+import path from "path"; // Import the path module
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import emailRoutes from "./routes/email.routes.js";
 import cartRoutes from "./routes/cart.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-
-
 import { connectDb } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 
-app.use (express.json());
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -27,18 +23,20 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("api/analytics", analyticsRoutes);
 
+// Manually define __dirname for ES modules
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
    app.use(express.static(path.join(__dirname, "frontend/dist")));
 
    app.get("*", (req, res) => {
        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-   })
+   });
 }
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running on PORT = " + process.env.PORT);
 
-const __dirname = path.resolve();
     // db connection
     connectDb();
-})  
+});
