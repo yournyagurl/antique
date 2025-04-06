43833e2ff2e5c30549d9dcb3ef840c2987348183
Sharon Boddu <elinia.sd@icloud.com>
2025-04-06 03:24:53 +0100

    wtf

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "https://antique-ej8u.onrender.com" : "/api",
+    baseURL: import.meta.env.MODE === "development"
+      ? "http://localhost:5000" // your local backend port
+      : "https://antique-ej8u.onrender.com", // your production API
     withCredentials: true,
-})
-
+  });
+  
 export default axiosInstance
