9fc10c29d5bf162bee7de8aad7dee56e640835a3
Sharon Boddu <elinia.sd@icloud.com>
2025-04-06 03:27:23 +0100

    api ending

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development"
-      ? "http://localhost:5000" // your local backend port
-      : "https://antique-ej8u.onrender.com", // your production API
+      ? "http://localhost:5000/api" // your local backend port
+      : "https://antique-ej8u.onrender.com/api", // your production API
     withCredentials: true,
   });
   
